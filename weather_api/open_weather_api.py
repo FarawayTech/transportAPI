b20689bae6b2461f9833576e424c0f077704f669
Roman Prokofyev <roman.prokofyev@gmail.com>
2020-12-31 07:30:17 +0100

    remove hot and cold ui, fix the elif

--- weather_api/open_weather_api.py
+++ weather_api/open_weather_api.py
@@ -15,19 +15,15 @@ class OpenWeatherApi(BaseWeatherApi):
     DAILY_FORECAST_URL = "https://api.openweathermap.org/data/2.5/onecall"
 
     @staticmethod
-    def _get_ui_config(forecast_dict: Dict, temperature: int) -> Dict:
+    def _get_ui_config(forecast_dict: Dict) -> Dict:
         main_type = ""
-        is_cold = True if temperature < 5 else False
-        is_hot = False
         intensity = 1
         weather_codes = set(str(w["id"]) for w in forecast_dict["weather"])
         main_codes = set(c[0] for c in weather_codes)
         if weather_codes.intersection({"615", "616"}):
             main_type = "mix-rain-snow"
-            is_cold = True
-        if weather_codes.intersection({"611", "612", "613"}):
+        elif weather_codes.intersection({"611", "612", "613"}):
             main_type = "sleet"
-            is_cold = True
         elif "2" in main_codes:
             if "212" in weather_codes:
                 main_type = "severe"
@@ -44,12 +40,10 @@ class OpenWeatherApi(BaseWeatherApi):
                 intensity = 1
             if weather_codes.intersection({"511"}):
                 intensity = 1
-                is_cold = True
             if weather_codes.intersection({"503", "504", "531"}):
                 intensity = 2
         elif "6" in main_codes:
             main_type = "snow"
-            is_cold = True
             if weather_codes.intersection({"600", "620"}):
                 intensity = 0.5
             if weather_codes.intersection({"601", "621"}):
@@ -67,13 +61,9 @@ class OpenWeatherApi(BaseWeatherApi):
         elif "801" in weather_codes:
             main_type = "cloud"
             intensity = 0.1
-            if temperature > 28:
-                is_hot = True
         elif "802" in weather_codes:
             main_type = "cloud"
             intensity = 0.3
-            if temperature > 28:
-                is_hot = True
         elif "803" in weather_codes:
             main_type = "cloud"
             intensity = 0.5
@@ -82,15 +72,9 @@ class OpenWeatherApi(BaseWeatherApi):
             intensity = 1
         elif "800" in weather_codes:
             main_type = "sun"
-            if temperature > 28:
-                is_hot = True
             intensity = 1
 
         classes = []
-        if is_cold:
-            classes.append("cold")
-        if is_hot:
-            classes.append("hot")
 
         # get name
         name = ", ".join(sorted(set(w["description"].title() for w in forecast_dict["weather"])))
@@ -117,6 +101,6 @@ class OpenWeatherApi(BaseWeatherApi):
         }
         result = requests.get(self.DAILY_FORECAST_URL, params=query).json()
         for d in result["daily"]:
-            d["ui_params"] = OpenWeatherApi._get_ui_config(d, d["temp"]["day"])
-        result["current"]["ui_params"] = OpenWeatherApi._get_ui_config(result["current"], result["current"]["temp"])
+            d["ui_params"] = OpenWeatherApi._get_ui_config(d)
+        result["current"]["ui_params"] = OpenWeatherApi._get_ui_config(result["current"])
         return result
